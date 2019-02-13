490c7ea43bfee8350663c82803dffa8328f61236
Benitp <b.kriel34@gmail.com>
2019-02-13 21:20:15 +0200

    this is displaying a number after the original master hello world

--- src/main/java/ac/za/cput/App.java
+++ src/main/java/ac/za/cput/App.java
@@ -1,5 +1,7 @@
 package ac.za.cput;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -9,7 +11,11 @@ public class App
     public static void main( String[] args )
 
     {
-        System.out.println( "Hello World!" );
+                                                System.out.println( "Hello World!" );
+                                                Scanner in = new Scanner(System.in);
+                                                System.out.println("Please enter a number");
+                                                int number = in.nextInt();
+                                                System.out.println(number);
 
     }
 }
